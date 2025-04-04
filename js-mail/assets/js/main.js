7ba9cd631f1a70382320635d604833cbf3ed2897
fedebacci <federicobacci1999@gmail.com>
2025-04-04 17:12:41 +0200

    modifica dell'esercizio con sfida di samuel

--- js-mail/assets/js/main.js
+++ js-mail/assets/js/main.js
@@ -4,20 +4,67 @@
 
 
 
+// * TODO
+// * USARE CONSOLE.DEBUG() INVECE DI ISDEBUG
+
+
+
+// * TODO
+// * FAR INSERIRE UN NUMERO DI EMAIL ALL'UTENTE PER FAR CREARE A LUI L'ARRAY DI EMAIL ACCETTATE
+    // * TRAMITE PIU CLICK DEL PULSANTE
+    // * TRAMITE UNA STRINGA SEPARATA DA VIRGOLE, DA TRASFORMARE IN ARRAY
+    // * TRAMITE PESCA CASUALE DI N EMAIL (DOVE N E NUMERO INSERITO DA UTENTE) DA UNA LISTA DI NOMI GIA CREATA
+    // * TRAMITE PESCA CASUALE DI N EMAIL (DOVE N E NUMERO GENERATO CASUALMENTE) DA UNA LISTA DI NOMI GIA CREATA
+    const listOfNames = [
+        "Alessia",
+        "Bernardo",
+        "Camilla",
+        "Domenico",
+        "Elena",
+        "Fabio",
+        "Ginevra",
+        "Harrison",
+        "Immacolata",
+        "Leonardo",
+        "Marta",
+        "Nicola",
+        "Olimpia",
+        "Paolo",
+        "Quincy",
+        "Raffaele",
+        "Simona",
+        "Tommaso",
+        "Umberta",
+        "Vincenzo",
+        "Zoe"
+    ]
+    const minNames = 1;
+    const maxNames = listOfNames.length - 1;
+    const numberOfNames = Math.floor(Math.random() * (maxNames - minNames + 1)) + minNames;
+    const acceptedEmails = [];
+    for (let i = 0; i <= numberOfNames; i ++) {
+        const currentName = listOfNames[i];
+        acceptedEmails.push(currentName)
+    }
+    console.table(acceptedEmails)
+
+
+    
 // * INFORMAZIONI COMUNI ALLE DUE VERSIONI DELL'ESERCIZIO
 const isDebug = true;
-const acceptedEmails = [
-    "classe144@email.it",
-    "donato@email.it",
-    "samuel@email.it",
-    "federico@email.it",
-    "test@email.it",
-    "altraemail@email.it"
-];
+// const acceptedEmails = [
+//     "classe144@email.it",
+//     "tiziano@email.it",
+//     "donato@email.it",
+//     "samuel@email.it",
+//     "federico@email.it",
+//     "test@email.it",
+//     "altraemail@email.it"
+// ];
 
 
 
-// * VERSIONE BASE SENZA PULSANTE CHE CHIEDE SUBITO L'EMAIL E CHE RICARICA AUTOMATICAMENTE LA PAGINA
+// // * VERSIONE BASE SENZA PULSANTE CHE CHIEDE SUBITO L'EMAIL E CHE RICARICA AUTOMATICAMENTE LA PAGINA
 // const userEmail = prompt("Inserisci la tua email", acceptedEmails[0]);
 // if (isDebug) {
 //     console.info(`Email inserita dall'utente: ${userEmail}`)
