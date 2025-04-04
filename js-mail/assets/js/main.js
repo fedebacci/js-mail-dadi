47cec98086396924637d4d1972a7c594d240c953
fedebacci <federicobacci1999@gmail.com>
2025-04-04 16:04:27 +0200

    sistemazione dei commenti del codice del primo esercizio

--- js-mail/assets/js/main.js
+++ js-mail/assets/js/main.js
@@ -2,6 +2,9 @@
 // * Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for. 
 // * Non è consentito usare nessun metodo proprio degli array (come includes, per esempio). Si può fare? Certo che si basta ragionare un po’. Nota: Non è necessario provvedere alla validazione delle email
 
+
+
+// * INFORMAZIONI COMUNI ALLE DUE VERSIONI DELL'ESERCIZIO
 const isDebug = true;
 const acceptedEmails = [
     "classe144@email.it",
@@ -11,6 +14,8 @@ const acceptedEmails = [
     "test@email.it",
     "altraemail@email.it"
 ];
+
+
 
 // * VERSIONE BASE SENZA PULSANTE CHE CHIEDE SUBITO L'EMAIL E CHE RICARICA AUTOMATICAMENTE LA PAGINA
 // const userEmail = prompt("Inserisci la tua email", acceptedEmails[0]);
