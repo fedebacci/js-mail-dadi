90f6137a8b1fd1fe9dade37ab0edd0b37ee28ad6
fedebacci <federicobacci1999@gmail.com>
2025-04-05 12:59:29 +0200

    svolgimento quarta versione sfida

--- js-mail/js-mail-7/assets/js/main.js
+++ js-mail/js-mail-7/assets/js/main.js
@@ -47,52 +47,37 @@
     
     // * VERSIONE CON PULSANTE PIÚ USER-FRIENDLY
     // * FATTA UTILIZZANDO METODI ARRAY APPROPRIATI
-    const btnAccessParty = document.getElementById('accessParty');
-    btnAccessParty.addEventListener('click', () => {
-        console.clear();
-        console.table(acceptedEmails);
-
-        console.log("acceptedEmails");
-        if (acceptedEmails.length !== 0) {
-            const userEmail = prompt("Inserisci la tua email", acceptedEmails[getRandomNumber(0, acceptedEmails.length - 1)]);
-            console.debug(`Email inserita dall'utente: ${userEmail}`);
-            console.debug("__________");
-            let emailFound = acceptedEmails.find((email) => email === userEmail);
-            console.debug(emailFound);
-            if (emailFound === undefined) {
-                console.info("Mi dispiace, non sei stato invitato!");
-                alert("Mi dispiace, non sei stato invitato!");
-            } else {
-                console.info("Puoi venire alla festa!");
-                alert("Puoi venire alla festa!");
-            };
-        } else {
-            alert("Non hai ancora creato una lista di invitati. Creala e riprova.")
-        };
-    });
-    
-
-    // * TODO
-    // * DECIDERE SE VOGLIO NASCONDERE IL PULSANTE ACCEDI ALLA FESTA OPPURE SE VOGLIO CONTROLLARE, QUANDO VIENE CLICCATO, LA PRESENZA DI INVITATI
     const btnNewList = document.getElementById('newList');
     btnNewList.addEventListener('click', generateListOfAcceptedEmails);
+    
     
     
     
     function getRandomNumber (max, min) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
-    // NB: ORA CHE NON LA ESEGUO PIU AL CARICAMENTO DELLA PAGINA NON SERVE AVERLA COME FUNZIONE generateListOfAcceptedEmails(), QUINDI LA SCRIVO DIRETTAMENTE DENTRO L'EVENT LISTENER DEL PULSANTE
+
+
+
+    // DIVERSAMENTE DA SFIDA PRECEDENTE LA RIMETTO COME FUNZIONE PER RICHIEDERE AUTOMATICAMENTE L'INPUT ALL'UTENTE IN CASO DI INPUT ERRATO
     function generateListOfAcceptedEmails() {
         console.clear();
         acceptedEmails.length = 0;
-        const numberOfNames = parseInt(prompt(`Quante persone vuoi invitare? (ricorda che la tua lista di email contiene ${listOfEmails.length} indirizzi)`, 1));
+        const userInput = prompt(`Quante persone vuoi invitare? (ricorda che la tua lista di email contiene ${listOfEmails.length} indirizzi)`, 1);
+        const numberOfNames = parseInt(userInput);
+        console.debug("______ userInput", userInput);
         console.debug("______ numberOfNames", numberOfNames);
 
-        if (numberOfNames === 0 || isNaN(numberOfNames)) {
+        
+        if (numberOfNames === 0 || userInput === null || (isNaN(numberOfNames) && userInput.length === 0)) {
             alert("Devi invitare qualcuno per fare una festa!");
+            generateListOfAcceptedEmails()
+        } else if (userInput.length !== numberOfNames.toString().length || isNaN(numberOfNames)) {
+            alert("È stato inserito qualcosa che non è un numero dentro l'input, riprova");
+            generateListOfAcceptedEmails()
         } else if (numberOfNames > listOfEmails.length) {
-            alert(`Non puoi invitare più di ${listOfEmails.length} persone perchè non hai abbastanza indirizzi a disposizione`)
+            alert(`Non puoi invitare più di ${listOfEmails.length} persone perchè non hai abbastanza indirizzi a disposizione`);
+            generateListOfAcceptedEmails()
         } else {
             while (acceptedEmails.length < numberOfNames) {
                 const currentName = listOfEmails[getRandomNumber(listOfEmails.length - 1, 0)];
@@ -102,9 +87,44 @@
             }
             acceptedEmails.sort();
             alert(`La lista di ${numberOfNames} invitati è stata generata`);
+
+            if (document.getElementById('accessParty') === null) {
+                const btnAccessPartyPlaceholder = document.getElementById('btnAccessPartyPlaceholder');
+                const btnAccessParty = document.createElement('button');
+                btnAccessParty.classList.add('btn', 'btn-primary');
+                btnAccessParty.id = "accessParty";
+                btnAccessParty.innerText = "Accedi alla festa";
+                console.log("btnAccessPartyPlaceholder", btnAccessPartyPlaceholder);
+                console.log("btnAccessParty", btnAccessParty);
+
+                btnAccessParty.addEventListener('click', () => {
+                    console.clear();
+                    console.table(acceptedEmails);
+            
+                    console.log("acceptedEmails");
+                    if (acceptedEmails.length !== 0) {
+                        const userEmail = prompt("Inserisci la tua email", acceptedEmails[getRandomNumber(0, acceptedEmails.length - 1)]);
+                        console.debug(`Email inserita dall'utente: ${userEmail}`);
+                        console.debug("__________");
+                        let emailFound = acceptedEmails.find((email) => email === userEmail);
+                        console.debug(emailFound);
+                        if (emailFound === undefined) {
+                            console.info("Mi dispiace, non sei stato invitato!");
+                            alert("Mi dispiace, non sei stato invitato!");
+                        } else {
+                            console.info("Puoi venire alla festa!");
+                            alert("Puoi venire alla festa!");
+                        };
+                    } else {
+                        alert("Non hai ancora creato una lista di invitati. Creala e riprova.")
+                    };
+                });
+
+                btnAccessPartyPlaceholder.appendChild(btnAccessParty);
+            }
+
             console.table(acceptedEmails);
             console.debug("acceptedEmails.length", acceptedEmails.length);
             console.debug("Il numero di email accettate è uguale al numero randomico estratto?", acceptedEmails.length === numberOfNames);
-        }
-
+        };
     };
